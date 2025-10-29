dd9fc61b14fc8b1c9018dc531e933e149e3eb14a
Ouaddou Abdellah <ouaddou.abdellah.topo@gmail.com>
2025-10-29 20:35:13 +0000

    إعداد ملف Firebase
    عادةً لديك ملف مثل firebase.ts أو firebaseClient.ts.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-3531262299-58293",
-  "appId": "1:691484846117:web:6526dfc69218b7e38177cc",
-  "apiKey": "AIzaSyB4t4v9eGsNqAf0BoKI4Ok44HUvZI55hZM",
-  "authDomain": "studio-3531262299-58293.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "691484846117"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
